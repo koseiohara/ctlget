8f066a4ecda9a946fe00ca12fc773ccb62dda37e
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2025-05-22 19:11:49 +0900

    temporary

--- ctlinfo.f90
+++ ctlinfo.f90
@@ -16,6 +16,7 @@ module ctlinfo
         private
         character(256) :: ctlname
         character(string_max), allocatable :: ctl_all(:)
+        integer :: lines
         integer :: dset
         integer :: title
         integer :: undef
@@ -28,21 +29,26 @@ module ctlinfo
 
         contains
 
+        procedure, nopass, private :: get_line_number
         procedure, pass, public  :: get_dset
         procedure, pass, public  :: get_title
         procedure, pass, public  :: get_undef
         procedure, pass, public  :: get_options
         procedure, pass, public  :: get_gridnum
-        procedure, pass, private :: get_n
-        !procedure, pass, public  :: get_lats
-        !procedure, pass, public  :: get_levs
         procedure, pass, public  :: get_nt
-        !procedure, pass, public  :: get_tini
-        !procedure, pass, public  :: get_dt
-        !procedure, pass, public  :: get_nvars
-        !procedure, pass, public  :: get_var_idx
+        procedure, pass, private :: get_n
+        procedure, pass, public  :: get_x
+        procedure, pass, public  :: get_y
+        procedure, pass, public  :: get_z
+        procedure, pass, private :: get_coordinate
+        procedure, pass, public  :: get_tini
+        procedure, pass, public  :: get_dt
+        procedure, pass, public  :: get_nvars
+        procedure, pass, public  :: get_var_idx
         !procedure, pass, public  :: get_var_description
         !procedure, pass, public  :: get_var_name
+        procedure, nopass, private :: get_ctl_dir
+        procedure, nopass, private :: skip_column
 
         final :: del
 
@@ -54,6 +60,7 @@ module ctlinfo
     end interface ctl
 
 
+ 
     contains
 
 
@@ -108,6 +115,7 @@ module ctlinfo
                 exit
             endif
         enddo
+        output%lines = lines
 
         ! delete spaces from left of each line
         output%ctl_all(1:lines) = adjustl(output%ctl_all(1:lines))
@@ -392,23 +400,224 @@ module ctlinfo
     end subroutine get_n
 
 
-    pure elemental function del_left_space(string) result(output)
-        character(*), intent(in) :: string
-        character(len(string))   :: output
-         
-        integer :: i
+    subroutine get_x(self, output)
+        class(ctl)  , intent(in)  :: self
+        real(real32), intent(out) :: output(:)
 
-        output = ''
-        do i = 1, len_trim(string)
-            ! get the first non-space character
-            if (string(i:i) == ' ') then
-                cycle
-            endif
-            output = trim(string(i:))
+        integer :: n
+
+        n = size(output)
+
+        call self%get_coordinate(self%xdef  , &  !! IN
+                               & n          , &  !! IN
+                               & output(1:n)  )  !! OUT
+
+    end subroutine get_x
+
+
+    subroutine get_y(self, output)
+        class(ctl)  , intent(in)  :: self
+        real(real32), intent(out) :: output(:)
+
+        integer :: n
+
+        n = size(output)
+
+        call self%get_coordinate(self%ydef  , &  !! IN
+                               & n          , &  !! IN
+                               & output(1:n)  )  !! OUT
+
+    end subroutine get_y
+
+
+    subroutine get_z(self, output)
+        class(ctl)  , intent(in)  :: self
+        real(real32), intent(out) :: output(:)
+
+        integer :: n
+
+        n = size(output)
+
+        call self%get_coordinate(self%zdef  , &  !! IN
+                               & n          , &  !! IN
+                               & output(1:n)  )  !! OUT
+
+    end subroutine get_z
+
+
+    subroutine get_coordinate(self, line_number, n, output)
+        class(ctl)  , intent(in)  :: self
+        integer     , intent(in)  :: line_number
+        integer     , intent(in)  :: n
+        real(real32), intent(out) :: output(n)
+
+        character(string_max)   :: line
+        character(string_max*2) :: line_levels
+        character(8)            :: specify_method
+        character(8)            :: n_c
+        real(real32)            :: min
+        real(real32)            :: delta
+        integer :: i
+        integer :: where_method
+        integer :: levels_start
+
+        line = self%ctl_all(line_number)
+        line = adjustl(line(5:string_max))
+
+        read(line,*) n_c, specify_method
+        if (to_lower(trim(specify_method)) == 'linear') then
+            ! if coordinate is 'linear', compute it
+            read(line,*) n_c, specify_method, min, delta
+            output(1:n) = [(min+delta*real(i, kind=real32), i = 0, n-1)]
             return
+        else
+            ! if coordinate is 'levels', concatenate def and the next line and get $n numbers of levels
+            where_method = index(to_lower(line), 'levels')
+            levels_start = where_method + 6
+            line_levels = trim(line) // ' ' // trim(self%ctl_all(line_number+1))
+            line_levels = line_levels(levels_start:)
+
+            read(line_levels,*) output(1:n)
+            return
+        endif
+
+    end subroutine get_coordinate
+
+
+    subroutine get_tini(self, calendar)
+        class(ctl), intent(in)  :: self
+        integer   , intent(out) :: calendar(5)
+
+        character(string_max) :: line
+        character(16) :: cal_str
+        character(4)  :: month_list(12) = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+        integer :: cal_len
+        integer :: where_month
+        integer :: where_z
+        integer :: where_colon
+        integer :: month
+
+        line = self%ctl_all(self%tdef)
+        cal_str = trim(skip_column(line, 4, ' '))
+        cal_str = to_lower(cal_str)
+        cal_len = len_trim(cal_str)
+
+        ! detect month
+        do month = 1, 12
+            where_month = index(cal_str, trim(month_list(month)))
+            if (where_month /= 0) then
+                where_month = where_month
+                ! get month
+                calendar(2) = month
+                exit
+            endif
         enddo
 
-    end function del_left_space
+        ! get year
+        read(cal_str(where_month+3:cal_len),*) calendar(1)
+        ! If only the last two digits of the year are written, convert them into a four-digit year
+        if (calendar(1) >= 50 .AND. calendar(1) <= 99) then
+            calendar(1) = calendar(1) + 1900
+        else if (calendar(1) >= 0 .AND. calendar(1) <= 49) then
+            calendar(1) = calendar(1) + 2000
+        endif
+
+        where_z = index(cal_str, 'z')
+        if (where_z == 0) then
+            ! if minute and hour are not written
+            if (where_month > 1) then
+                ! if day is written
+                read(cal_str(:where_month-1),*) calendar(3)
+            else
+                calendar(3) = 1
+            endif
+            calendar(4) = 0
+            calendar(5) = 0
+        else
+            ! get day
+            read(cal_str(where_z+1:where_month-1),*) calendar(3)
+            read(cal_str(1:2),*) calendar(4)
+            where_colon = index(cal_str, ':')
+            if (where_colon == 0) then
+                ! if minute is not written
+                calendar(5) = 0
+            else
+                read(cal_str(where_colon+1:where_z-1),*) calendar(5)
+            endif
+        endif
+
+    end subroutine get_tini
+
+
+    subroutine get_dt(self, dt, unit)
+        class(ctl)  , intent(in)  :: self
+        integer     , intent(out) :: dt
+        character(*), intent(out), optional :: unit
+
+        character(string_max) :: line
+        character(4)          :: dt_c
+        integer               :: dt_len
+
+        ! get delta_t column in lower cases
+        line = self%ctl_all(self%tdef)
+        dt_c = trim(skip_column(line, 5, ' '))
+        dt_c = to_lower(dt_c)
+
+        dt_len = len_trim(dt_c)
+        ! get integer delta
+        read(dt_c(1:dt_len-2),*) dt
+
+        ! get unit of delta
+        if (present(unit)) then
+            unit = dt_c(dt_len-1:dt_len)
+        endif
+
+    end subroutine get_dt
+
+
+    subroutine get_nvars(self, output)
+        class(ctl), intent(in)  :: self
+        integer   , intent(out) :: output
+
+        character(string_max) :: line
+        integer :: n_end
+
+        line = self%ctl_all(self%vars)
+
+        ! trimming
+        line = adjustl(line(5:string_max))
+        n_end = index(line(1:string_max), '*') - 1
+        ! if space is not found, the last character is selected
+        if (n_end == -1) then
+            n_end = string_max
+        endif
+        line = trim(line(1:n_end))
+
+        read(line,*) output
+
+    end subroutine get_nvars
+
+
+    subroutine get_var_idx(self, var, output)
+        class(ctl)  , intent(in)  :: self
+        character(*), intent(in)  :: var
+        integer     , intent(out) :: output
+
+        ! get the line $var is defined in
+        call get_line_number(var                       , &
+                           & self%lines                , &
+                           & self%ctl_all(1:self%lines), &
+                           & output                      )
+
+        if (output == 0) then
+            write(0,'(A)') '<ERROR STOP>'
+            write(0,'(A)') trim(var) // ' was not found in ' // trim(self%ctlname)
+            ERROR STOP
+        endif
+
+        output = output - self%vars
+
+    end subroutine get_var_idx
 
 
     subroutine get_ctl_dir(ctlname, ctldir)
@@ -421,6 +630,48 @@ module ctlinfo
         ctldir = ctlname(1:where_slash)
 
     end subroutine get_ctl_dir
+
+
+    pure elemental function skip_column(input, target_column, delimiter) result(output)
+        character(*), intent(in) :: input
+        integer     , intent(in) :: target_column
+        character(*), intent(in) :: delimiter
+        character(len(input)) :: output
+        character(len(input)) :: input_cp
+        integer :: column_end
+        integer :: column
+
+        input_cp = adjustl(input)
+        column_end = index(input_cp, '*') - 1
+        ! delete comments
+        if (column_end /= -1) then
+            input_cp = input_cp(1:column_end)
+        endif
+            
+        column_end = index(input_cp, delimiter) - 1
+        ! delimiter was not found => end
+        if (column_end == -1) then
+            output = input_cp
+            return
+        endif
+
+        do column = 1, len(input)
+            output = adjustl(input_cp(1:column_end))
+            !if (column_end == 0) then
+            !    output = ''
+            !endif
+            
+            if (column == target_column) then
+                return
+            endif
+            input_cp = adjustl(input_cp(column_end+2:))
+            column_end = index(input_cp, delimiter) - 1
+        enddo
+
+        ! Error
+        output = ''
+
+    end function skip_column
 
 
 end module ctlinfo
--- test.f90
+++ test.f90
@@ -16,6 +16,17 @@ program test
     integer :: ny
     integer :: nz
     integer :: nt
+    real(real32), allocatable :: x(:)
+    real(real32), allocatable :: y(:)
+    real(real32), allocatable :: z(:)
+
+    integer :: calendar(5)
+    integer :: dt
+    character(8) :: dt_unit
+    integer :: vars
+
+    character(16) :: var
+    integer :: idx
 
     ctlname='JRA3Q_1990_2020_ZONAL_366.ctl'
 
@@ -29,6 +40,10 @@ program test
     call input%get_gridnum(nx=nx, ny=ny, nz=nz)
     call input%get_nt(nt)
 
+    allocate(x(nx))
+    allocate(y(ny))
+    allocate(z(nz))
+
     write(*,'(A)') 'FILE : ' // trim(filename)
     write(*,'(A)') 'TITLE : ' // trim(title)
     write(*,'(A,ES0.7)') 'UNDEF : ', undef
@@ -38,6 +53,33 @@ program test
     write(*,'(A,I0)') 'NY : ', ny
     write(*,'(A,I0)') 'NZ : ', nz
     write(*,'(A,I0)') 'NT : ', nt
+
+    if ('GrADS' == 'grads') then
+        write(*,*) 'GrADS = grads'
+    else
+        write(*,*) 'GrADS /= grads'
+    endif
+
+    call input%get_x(x(1:nx))
+    write(*,'(A,*(ES0.3,:,", "))') 'X : ', x(1:nx)
+    call input%get_y(y(1:ny))
+    write(*,'(A,*(ES0.3,:,", "))') 'Y : ', y(1:ny)
+    call input%get_z(z(1:nz))
+    write(*,'(A,*(ES0.3,:,", "))') 'Z : ', z(1:nz)
+
+    call input%get_tini(calendar(1:5))
+    write(*,'(*(I0,:,"/"))') calendar(1:5)
+
+    call input%get_dt(dt)
+    write(*,'(I0,A)') dt, trim(dt_unit)
+
+    call input%get_nvars(vars)
+    write(*,'(A,I0)') 'VARS : ', vars
+
+    var = 'dkedt_vke'
+    call input%get_var_idx(var, idx)
+    write(*,'(A,I0)') 'index of ' // trim(var) // ' is ', idx
+
 
 end program test
 
